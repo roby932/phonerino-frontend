693f02d393eed5cbf7d1f4f814c0f7148e3ae182
Robert-Constantin Dumitrescu <robert.dumitrescu@padawan.devacademy.ro>
2015-10-18 22:37:16 +0300

    ceva

---
+++ app/app.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+// var React = require('react');
+// var ReactDom = require('react-dom');
+// require('./app.scss');
+// require('hint.css/src/hint.scss');
+
+// // set global for query for bootstrap
+// import jQuery from 'jquery';
+// window.jQuery = window.$ = jQuery;
+// require('./bootstrap.js');
+
+ReactDOM.render((
+  <div>
+    Hello world!
+  </div>
+), document.getElementById('app-container'));
